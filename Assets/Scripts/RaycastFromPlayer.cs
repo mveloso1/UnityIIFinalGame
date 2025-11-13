a98337c3f10087de96cb6ce1100b467446b8723e
Fish-sticks-56 <lrfish@email.neit.edu>
2025-11-13 11:16:51 -0500

    Added BehaviorOnShot
    Added BehaviorOnShot and RemoveObjBehavior, made platform in tutorial disappear

--- Assets/Scripts/RaycastFromPlayer.cs
+++ Assets/Scripts/RaycastFromPlayer.cs
@@ -9,24 +9,24 @@ public class RaycastFromPlayer : MonoBehaviour
     bool holdingItem = false;
     GameObject heldObj;
 
-    public bool redBox = false;
-    public bool blueBox = false;
-    public bool prism = false;
+    //public bool redBox = false;
+    //public bool blueBox = false;
+    //public bool prism = false;
 
-    public GameObject doorButton;
-    public Animator leftDoor;
-    public Animator rightDoor;
+    //public GameObject doorButton;
+    //public Animator leftDoor;
+    //public Animator rightDoor;
     public Animator templeDoor;
 
-    bool doorUnlocked = false;
+    //bool doorUnlocked = false;
     bool templeDoorUnlocked = false;
 
     public AudioClip puzzleSolve;
     private AudioSource audioSource;
 
     MeshRenderer hitObj;
-    public GameObject messageBox;
-    public GameObject bow;
+    //public GameObject messageBox;
+    //public GameObject bow;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -39,56 +39,57 @@ public class RaycastFromPlayer : MonoBehaviour
     {
         Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.green);
 
-        if(redBox && blueBox) //&& prism
-        {
-           // doorButton.GetComponent<Renderer>().material.color = Color.green;
-            doorUnlocked = true;
-            templeDoorUnlocked = true;
-           // AudioSource.PlayClipAtPoint(puzzleSolve, transform.position);
+        //if(redBox && blueBox) //&& prism
+        //{
+        //   // doorButton.GetComponent<Renderer>().material.color = Color.green;
+        //    doorUnlocked = true;
+        //    templeDoorUnlocked = true;
+        //   // AudioSource.PlayClipAtPoint(puzzleSolve, transform.position);
 
-        }
-        else
-        {
-            //doorButton.GetComponent<Renderer>().material.color = Color.red;
-            doorUnlocked = false;
-            templeDoorUnlocked = false;
-        }
+        //}
+        //else
+        //{
+        //    //doorButton.GetComponent<Renderer>().material.color = Color.red;
+        //    doorUnlocked = false;
+        //    templeDoorUnlocked = false;
+        //}
 
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, transform.forward, out hit, 3.0f))
         {
-            if (hit.collider.tag == "PickupItem" && !holdingItem)
-            {
-                hitObj = hit.collider.GetComponent<MeshRenderer>();
-                hitObj.materials[1].SetFloat("_Scale", 1.03f);
-            }
-            if(hit.collider.tag == "DoorButton" && doorUnlocked)
-            {
-                hitObj = hit.collider.GetComponent<MeshRenderer>();
-                hitObj.materials[1].SetFloat("_Scale", 1.03f);
-                messageBox.SetActive(true);
-            }
-            else if (hit.collider.tag == "Archer")
-            {
-                messageBox.SetActive(true);
-            }
-            else
-            {
-                messageBox.SetActive(false);
-            }
+            //if (hit.collider.tag == "PickupItem" && !holdingItem)
+            //{
+            //    hitObj = hit.collider.GetComponent<MeshRenderer>();
+            //    hitObj.materials[1].SetFloat("_Scale", 1.03f);
+            //}
+            //if(hit.collider.tag == "DoorButton" && doorUnlocked)
+            //{
+            //    hitObj = hit.collider.GetComponent<MeshRenderer>();
+            //    hitObj.materials[1].SetFloat("_Scale", 1.03f);
+            //    messageBox.SetActive(true);
+            //}
+            //else if (hit.collider.tag == "Archer")
+            //{
+            //    messageBox.SetActive(true);
+            //}
+            //else
+            //{
+            //    messageBox.SetActive(false);
+            //}
+            
         }
         else
         {
-            if (hitObj != null)
-            {
-                hitObj.materials[1].SetFloat("_Scale", 1.0f);
-                hitObj = null;
-                if(messageBox.activeSelf)
-                {
-                    messageBox.SetActive(false);
-                }
-            }
+            //if (hitObj != null)
+            //{
+            //    hitObj.materials[1].SetFloat("_Scale", 1.0f);
+            //    hitObj = null;
+            //    if(messageBox.activeSelf)
+            //    {
+            //        messageBox.SetActive(false);
+            //    }
+            //}
         }
 
     }
@@ -127,17 +128,26 @@ public class RaycastFromPlayer : MonoBehaviour
 
         if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance))
         {
-           
-            if (hit.collider.CompareTag("DoorButton") && doorUnlocked)
+           // 
+           if(hit.collider.gameObject.TryGetComponent<BehaviorOnShot>(out BehaviorOnShot behaviorScript))
             {
-                leftDoor.SetTrigger("OpenDoor");
-                rightDoor.SetTrigger("OpenDoor");
+                behaviorScript.Shot();
+                Debug.Log(hit.collider.name + " was hit successfully!");
             }
-            if (hit.collider.CompareTag("Archer"))
-            {
-                //toggle 
-                bow.SetActive(true);
-            }
+            else
+                Debug.Log(hit.collider.name + " was hit, did not trigger!");
+
+            //if (hit.collider.CompareTag("DoorButton") && doorUnlocked)
+            //{
+            //    leftDoor.SetTrigger("OpenDoor");
+            //    rightDoor.SetTrigger("OpenDoor");
+            //}
+            //if (hit.collider.CompareTag("Archer"))
+            //{
+            //    //toggle 
+            //    bow.SetActive(true);
+            //}
+        
         }
     }
 }
--- Assets/Scripts/TriggerPlate.cs
+++ Assets/Scripts/TriggerPlate.cs
@@ -10,18 +10,18 @@ public class TriggerPlate : MonoBehaviour
     {
         if (other.gameObject.name == objTag)
         {
-            if (other.gameObject.name == "RedBox")
-            {
-                raycastScript.redBox = true;
-            }
-            if (other.gameObject.name == "BlueBox")
-            {
-                raycastScript.blueBox = true;
-            }
-            if (other.gameObject.name == "ThePrism")
-            {
-                raycastScript.prism = true;
-            }
+            //if (other.gameObject.name == "RedBox")
+            //{
+            //    raycastScript.redBox = true;
+            //}
+            //if (other.gameObject.name == "BlueBox")
+            //{
+            //    raycastScript.blueBox = true;
+            //}
+            //if (other.gameObject.name == "ThePrism")
+            //{
+            //    raycastScript.prism = true;
+            //}
 
         }
     }
@@ -30,18 +30,18 @@ public class TriggerPlate : MonoBehaviour
     {
         if (other.gameObject.name == objTag)
         {
-            if (other.gameObject.name == "RedBox")
-            {
-                raycastScript.redBox = false;
-            }
-            if (other.gameObject.name == "BlueBox")
-            {
-                raycastScript.blueBox = false;
-            }
-            if (other.gameObject.name == "ThePrism")
-            {
-                raycastScript.prism = false;
-            }
+            //if (other.gameObject.name == "RedBox")
+            //{
+            //    raycastScript.redBox = false;
+            //}
+            //if (other.gameObject.name == "BlueBox")
+            //{
+            //    raycastScript.blueBox = false;
+            //}
+            //if (other.gameObject.name == "ThePrism")
+            //{
+            //    raycastScript.prism = false;
+            //}
 
         }
     }
