16a2527b50d48356137e495acc6a1015489f2c03
mveloso1 <mveloso1@email.neit.edu>
2025-12-11 22:49:02 -0500

    fixes

--- Assets/Scripts/CreditsTimer.cs
+++ Assets/Scripts/CreditsTimer.cs
@@ -1,54 +1,20 @@
-using TMPro;
+using System.Collections;
 using UnityEngine;
-using System;
-using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
-public class CreditsTimer : MonoBehaviour
+
+public class SceneTimer : MonoBehaviour
 {
-    public float timeRemaining;
-    int minutes;
-    int seconds;
-    public TMP_Text timer;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float delay = 8f;         // time before switching scenes
+    public int sceneToLoad = 0;      // build index OR replace with string for scene name
+
+    private void Start()
     {
-        timeRemaining = 10;
+        StartCoroutine(LoadSceneAfterDelay());
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private IEnumerator LoadSceneAfterDelay()
     {
-        CountDown();
-    }
-
-    void CountDown()
-    {
-        if (timeRemaining > 0)
-        {
-            timeRemaining -= Time.deltaTime;
-            minutes = Mathf.FloorToInt(timeRemaining / 60f);
-            seconds = Mathf.FloorToInt(timeRemaining % 60f);
-
-            if (seconds < 10)
-            {
-                timer.text = $"Time left {minutes}:0{seconds}";
-            }
-            else
-            {
-                timer.text = $"Time left {minutes}:{seconds}";
-            }
-        }
-        else
-        {
-            timeRemaining = 0;
-            timer.text = "Time left 0:00";
-            SceneManager.LoadScene(1);
-        }
-    }
-
-    private void TimeIsUp()
-    {
-        //Game over
-        Debug.Log("Time is up!");
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
